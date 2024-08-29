3a0718f370895becc2815c8aa4fa878c98d7d6a3
eralonso <eralonso@car11s2.42barcelona.com>
2024-08-29 16:51:12 +0200

    Making read tests

--- src/main.c
+++ src/main.c
@@ -36,6 +36,7 @@ size_t	ft_strlen(const char *str);
 char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
 //{
 //	size_t	len = 0;
 //
@@ -173,26 +174,50 @@ void	test_write(void)
 	}
 }
 
+void	__test_read_time(ssize_t (*read_pointer)(int, void *, size_t), char *name, int fd, char *buf, size_t count)
+{
+	unsigned long	start;
+	unsigned long	end;
+	ssize_t			written;
+
+	start = get_time();
+	written = read_pointer(fd, buf, count);
+	end = get_time();
+	printf("%s(%i, %s, %zu) == %zd && errno == %i, buf == %s, in %lu microseconds\n", name, fd, "test", count, written, errno, buf, end - start);
+}
+
+void	test_read(void)
+{
+	size_t	strs_size[] = {1, 10, 100};
+	char	*strs[] = {calloc(strs_size[0], sizeof(char)), calloc(strs_size[1], sizeof(char)), calloc(strs_size[2], sizeof(char))};
+	int		fd[] = {0, -1};
+	int		strs_len = sizeof(strs) / sizeof(*strs);
+	int		fd_len = sizeof(fd) / sizeof(*fd);
+
+	printf("TEST: READ vs FT_READ\n");
+	for (int i = 0; i < fd_len; i++)
+	{
+		for (int j = 0; j < strs_len; j++)
+		{
+			printf("TEST %i:\n", (i * strs_len) + j + 1);
+			__test_read_time(read, "\tread", fd[i], strs[j], strs_size[j] - 1);
+			__test_read_time(ft_read, "\tft_read", fd[i], strs[j], strs_size[j] - 1);
+		}
+	}
+	for (int i = 0; i < strs_len; i++)
+		free(strs[i]);
+}
+
 int	main(void)
 {
-	void	(*tests[])(void) = {test_strlen, test_strcpy, test_strcmp, test_write};
+	void	(*tests[])(void) = {test_strlen, test_strcpy, test_strcmp, test_write, test_read};
 	int		tests_size = sizeof(tests) / sizeof(*tests);
 
-	for	(int test = 3; test < tests_size; test++)
+	for	(int test = 4; test < tests_size; test++)
 	{
 		tests[test]();
 		if (test < tests_size - 1)
 			printf("\n\n");
 	}
-	//printf("%i %s\n", EFAULT, strerror(EFAULT));
-	//size_t	len = 0;
-	//char	b[20];
-	//test_strlen();
-	//printf("\n\n");
-	//test_strcpy();
-	//printf("\n\n");
-	//test_strcmp();
-	//ft_strcpy(b, "aaa");
-	//len = ft_strlen("sadasdsada");
 	return (0);
 }
