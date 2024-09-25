b6ee7abbfed7cfdb06f53b2b5d43a53ea6b5897f
eralonso <eralonso@cbr7s4.42barcelona.com>
2024-09-25 17:13:49 +0200

    Finished quick sort algorithm c implementation

--- c/quicksort.c
+++ c/quicksort.c
@@ -57,70 +57,104 @@ char	*get_string_bool(short expr)
 	return (expr == 1 ? "true" : "false");
 }
 
-void	int_arr_swap(int *arr, size_t i1, size_t i2)
+void	int_swap(int *a, int *b)
 {
-	arr[i1] ^= arr[i2];
-	arr[i2] ^= arr[i1];
-	arr[i1] ^= arr[i2];
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 	return ;
 }
 
-size_t	qs__get_optimal_pivot(int *arr, size_t init, size_t end)
+int	qs__get_optimal_pivot(int *arr, int init, int end)
 {
-	size_t	pivot_index;
-	size_t	mid_size;
-	size_t	i;
+	int	pivot_index;
+	int	mid_size;
+	int	i;
+	int	tmp;
 
 	mid_size = (end - init) / 2;
 	i = init;
 	pivot_index = init;
+	tmp = int_end;
 	while (i < mid_size)
 	{
 		if (arr[i] > arr[pivot_index])
-			pivot_index++;
+			pivot_index = i;
 		i++;
 	}
 	return (pivot_index);
 }
 
-size_t	qs__set_rigth_place_pivot(int *arr, size_t init, size_t end, size_t pivot_index)
+int	qs__set_rigth_place_pivot(int *arr, int init, int end, int pivot_index)
 {
-	size_t	i;
-	size_t	j;
+	int	i;
+	int	j;
+	int	pivot;
 
 	i = init;
 	j = end;
+	pivot = arr[pivot_index];
 	while (i < j)
 	{
-		while (arr[i] < arr[pivot_index])
+		while (arr[i] <= pivot && i <= end - 1)
 			i++;
-		while (j > 0 && arr[j] > arr[pivot_index])
+		while (arr[j] > pivot && j >= init + 1)
 			j--;
-		if (arr[i] > arr[j])
-		{
-			int_arr_swap(arr, i, j);
-			if (i == pivot_index)
-				pivot_index = j;
-			else if (j == pivot_index)
-				pivot_index = i;
-		}
-		else
+		if (i >= j)
 			break ;
+		int_swap(arr + i, arr + j);
+		if (i == pivot_index)
+			pivot_index = j;
+		else if (j == pivot_index)
+			pivot_index = i;
 	}
-	return (pivot_index);
+	int_swap(arr + pivot_index, arr + j);
+	return (j);
 }
 
-void	qs__loop(int *arr, size_t init, size_t end)
+// 1: 8 3 4 7 2 1 5 9 6 -> low = 0 | high = 8
+// pivot: 8
+// 8 3 4 7 2 1 5 6 9
+// 6 3 4 7 2 1 5 8 9
+// pivot index: 7
+// 2.1: 6 3 4 7 2 1 5 -> low = 0 | high = 6
+// pivot: 6
+// 6 3 4 5 2 1 7
+// 1 3 4 5 2 6 7
+// pivot index: 5
+// 2.2: 9 -> low = 8 | high = 8 // Because low isn't lower than high then it's already sort
+// 2.1.1: 1 3 4 5 2 -> low = 0 | high = 4
+// pivot: 1
+// 1 3 4 5 2 // There isn't a lower number than pivot
+// pivot index: 0
+// 2.1.2: 7 -> low = 6 | high = 6 // Because low isn't lower than high then it's already sort
+// 2.1.1.1: Out Of Range -> low = 0 | high = -1 // Because low isn't lower than high then it's not neccessary sort
+// 2.1.1.2: 3 4 5 2 -> low = 1 | high = 4
+// pivot: 3
+// 3 2 5 4
+// 2 3 5 4
+// pivot index: 2
+// 2.1.1.2.1: 2 -> low = 1 | high = 1 // Because low isn't lower than high then it's already sort
+// 2.1.1.2.2: 5 4 -> low = 3 | high = 4
+// pivot: 5
+// 4 5 // This swap is made outside the while loop
+// pivot index: 4
+// 2.1.1.2.2.1: 4 -> low = 3 | high = 3 // Because low isn't lower than high then it's already sort
+// 2.1.1.2.2.2: 6 -> low = 5 | high = 4 // Because low isn't lower than high then it's already sort
+
+void	qs__loop(int *arr, int init, int end)
 {
-	size_t	pivot_index;
+	int	pivot_index;
 
 	if (init < end)
 	{
 		pivot_index = qs__get_optimal_pivot(arr, init, end);
-		printf("pivot index: %zu\n", pivot_index);
+		// printf("pivot index: %zu\n", pivot_index);
 		pivot_index = qs__set_rigth_place_pivot(arr, init, end, pivot_index);
-		qs__loop(arr, init, pivot_index);
-		qs__loop(arr, pivot_index + 1, end);		
+		qs__loop(arr, init, pivot_index - 1);
+		qs__loop(arr, pivot_index + 1, end);
 	}
 	return ;
 }
@@ -136,8 +170,15 @@ void	quicksort(int *arr)
 
 void	test_quicksort(void)
 {
-	int	n[3][100] = {{9, 1, 8, 2, -4, int_end}, {0, 1, int_end}, {-1, -1, int_end}};
-	int	n_arr_amount = sizeof(n) / sizeof(*n);
+	int		n[5][100] = {
+		{9, 1, 8, 2, -4, int_end},
+		{0, 1, int_end},
+		{-1, -1, int_end},
+		{1, 34, 6, 76, 23, -1, -2147483647, 2147483647, 34, 1, 43, int_end},
+		{1234, 12345, 12, 123456, 234, 456, -123, 123453, -1234, int_end}
+	};
+	int		n_arr_amount = sizeof(n) / sizeof(*n);
+	short	is_sorted;
 
 	for (int test = 0; test < n_arr_amount; test++)
 	{
@@ -146,7 +187,8 @@ void	test_quicksort(void)
 		quicksort(n[test]);
 		printf("AFTER:\n");
 		print_array_numbers(n[test]);
-		printf("is_sorted: %s\n", get_string_bool(int_arr_issorted(n[test])));
+		is_sorted = int_arr_issorted(n[test]);
+		printf("is_sorted: \033[1;9%cm%s\033[0m\n", is_sorted == 1 ? '2' : '1', get_string_bool(is_sorted));
 		printf("\n");
 	}
 	return ;
